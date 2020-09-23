7cd5449d4268cf442741e74f62fb2691ed0fca17
Juan Orjuela <orjuelajuans@gmail.com>
2020-09-22 19:11:47 -0500

    Mapa v1

---
+++ assets/js/locations.js
@@ -0,0 +1,18 @@
+var locations = [
+    ['LUAR', "img/logos/logo-luar.png", "215.000.000", 4.739489, -74.064520, "#", "img/fachadas/fachada-luar.jpg", 56.75, 75.34, "Bogotá", "Salitre", "Cra 3a # 30 - 10", "img/estados/construccion.svg"],
+    ['LUAR', "img/logos/logo-luar.png", "215.000.000", 4.738259, -74.067170, "#", "img/fachadas/fachada-luar.jpg", 56.75, 75.34, "Bogotá", "Salitre", "Cra 3a # 30 - 10", "img/estados/construccion.svg"],
+    ['LUAR', "img/logos/logo-luar.png", "215.000.000", 4.732421, -74.069713, "#", "img/fachadas/fachada-luar.jpg", 56.75, 75.34, "Bogotá", "Salitre", "Cra 3a # 30 - 10", "img/estados/construccion.svg"],
+    ['LUAR', "img/logos/logo-luar.png", "215.000.000", 4.718013, -74.110359, "#", "img/fachadas/fachada-luar.jpg", 56.75, 75.34, "Bogotá", "Salitre", "Cra 3a # 30 - 10", "img/estados/construccion.svg"],
+    ['LUAR', "img/logos/logo-luar.png", "215.000.000", 4.724682, -74.135046, "#", "img/fachadas/fachada-luar.jpg", 56.75, 75.34, "Bogotá", "Salitre", "Cra 3a # 30 - 10", "img/estados/construccion.svg"],
+    ['LUAR', "img/logos/logo-luar.png", "215.000.000", 4.671474, -74.139490, "#", "img/fachadas/fachada-luar.jpg", 56.75, 75.34, "Bogotá", "Salitre", "Cra 3a # 30 - 10", "img/estados/construccion.svg"],
+    ['LUAR', "img/logos/logo-luar.png", "215.000.000", 4.728196, -74.038651, "#", "img/fachadas/fachada-luar.jpg", 56.75, 75.34, "Bogotá", "Salitre", "Cra 3a # 30 - 10", "img/estados/construccion.svg"],
+    ['LUAR', "img/logos/logo-luar.png", "215.000.000", 4.760251, -74.034424, "#", "img/fachadas/fachada-luar.jpg", 56.75, 75.34, "Bogotá", "Salitre", "Cra 3a # 30 - 10", "img/estados/construccion.svg"],
+    ['LUAR', "img/logos/logo-luar.png", "215.000.000", 4.657666, -74.057721, "#", "img/fachadas/fachada-luar.jpg", 56.75, 75.34, "Bogotá", "Salitre", "Cra 3a # 30 - 10", "img/estados/construccion.svg"],
+    ['LUAR', "img/logos/logo-luar.png", "215.000.000", 4.594487, -74.104251, "#", "img/fachadas/fachada-luar.jpg", 56.75, 75.34, "Bogotá", "Salitre", "Cra 3a # 30 - 10", "img/estados/construccion.svg"],
+    ['LUAR', "img/logos/logo-luar.png", "215.000.000", 4.625824, -74.147079, "#", "img/fachadas/fachada-luar.jpg", 56.75, 75.34, "Bogotá", "Salitre", "Cra 3a # 30 - 10", "img/estados/construccion.svg"],
+    ['LUAR', "img/logos/logo-luar.png", "215.000.000", 3.384574, -76.528535, "#", "img/fachadas/fachada-luar.jpg", 56.75, 75.34, "Bogotá", "Salitre", "Cra 3a # 30 - 10", "img/estados/construccion.svg"],
+    ['LUAR', "img/logos/logo-luar.png", "215.000.000", 3.390121, -76.541692, "#", "img/fachadas/fachada-luar.jpg", 56.75, 75.34, "Bogotá", "Salitre", "Cra 3a # 30 - 10", "img/estados/construccion.svg"],
+    ['LUAR', "img/logos/logo-luar.png", "215.000.000", 3.398346, -76.532158, "#", "img/fachadas/fachada-luar.jpg", 56.75, 75.34, "Bogotá", "Salitre", "Cra 3a # 30 - 10", "img/estados/construccion.svg"],
+    
+     
+];
